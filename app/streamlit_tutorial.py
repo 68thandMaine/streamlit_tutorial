d44a09829ae2280faa75a702bd83227dfff5f080
Chris Rudnicky <crudnicky@absci.com>
2023-09-20 20:26:21 -0600

    feat: add uber pickup app

---
+++ app/streamlit_tutorial.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.header("Streamlit Tutorial")
+st.write("This project contains the tutorial from the Streamlit docs.")
+st.write("There are two main projects in this app. One is for covering the main concepts, and the other is for the Uber Pickup app.")
